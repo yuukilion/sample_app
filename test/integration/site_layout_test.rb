02588dd658545afc921cfaedeea664515cfe5d92
yuukilion <hy130310@gmail.com>
2023-08-14 06:43:48 +0000

    [add]Signupページを追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,6 +8,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
     get contact_path
     assert_select "title", full_title("Contact")
   end
