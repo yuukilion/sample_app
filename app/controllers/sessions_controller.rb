c67320bd081318aec7c175d1bf7c71c829e399f3
yuukilion <hy130310@gmail.com>
2023-08-17 08:10:23 +0000

    コードの微調整

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:sessions][:password])
       reset_session
-      log_in(user)
+      log_in user
       redirect_to user
     else
       flash.now[:danger] = "todo"
