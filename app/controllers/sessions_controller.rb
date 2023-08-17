f45c09c49dea8948f652800683f735b8c4f5201b
yuukilion <hy130310@gmail.com>
2023-08-17 07:57:54 +0000

    [add]暫定的なログイン処理

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      #todo
+    else
+      #エラーメッセージを追加
+      flash[:danger] = "todo"
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
