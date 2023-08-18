0d91068ef988c6e40840ade1767f3f1d7fa03f57
yuukilion <hy130310@gmail.com>
2023-08-18 13:39:44 +0000

    [fix]正常に動作しないため原因である空白を削除

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,10 +1,10 @@
 //メニュー操作
 
-document.addEventListener("turbo: load", function() {
+document.addEventListener("turbo:load", function() {
   let account = document.querySelector("#account");
-  account.addEventListener("click",function(e){
+  account.addEventListener("click", function(e){
     e.preventDefault();
     let menu = document.querySelector("#dropdown-menu");
     menu.classList.toggle("active");
-  })
+  });
 });
