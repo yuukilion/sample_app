9edbc7b1047fb1f691273c89ed54998370d89998
yuukilion <hy130310@gmail.com>
2023-08-24 08:54:07 +0000

    [add]更新処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit', status: :unprocessable_entity
     end
