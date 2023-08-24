6b507b91c0f6cfdad56711d1cbc6badd6ce7b2d4
yuukilion <hy130310@gmail.com>
2023-08-24 04:41:55 +0000

    [add]Remember meの修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user &.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user &.authenticate(params[:session][:password])
       reset_session
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      log_in user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to @user
     else
       flash.now[:danger] = "todo"
       render 'new', status: :unprocessable_entity
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -75,3 +75,19 @@ class LogoutTest < Logout
     assert_redirected_to root_url
   end
 end
+
+class RememberingTest < UsersLogin
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    # Cookieを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    # Cookieが削除されていることを検証してからログイン
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
+end
