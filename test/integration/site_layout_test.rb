58c29c3a325faa69cfb9d33992de9a96b132a35c
yuukilion <hy130310@gmail.com>
2023-08-25 08:29:01 +0000

    [add]ユーザー一覧を追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "layout links without login" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -12,5 +16,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Sign up")
     get contact_path
     assert_select "title", full_title("Contact")
+  end
+
+  test "layout links after login" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
   end
 end
