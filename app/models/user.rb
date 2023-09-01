e32a4a8e2b02211e2ec37cdd4a4a856ccc6d2440
yuukilion <hy130310@gmail.com>
2023-09-01 11:08:31 +0000

    [add]User用のMailerを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,6 +50,6 @@ class User < ApplicationRecord
 
     def create_activation_digest
       self.activation_token  = User.new_token
-      self.actication_digest = User.digest(self.activation_token)
+      self.activation_digest = User.digest(self.activation_token)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(
   email: "example@railstutorial.org",
   password:              "foobar",
   password_confirmation: "foobar",
-  admin: true
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 # 追加のサンプルユーザーをまとめて生成する
@@ -14,5 +16,8 @@ User.create!(
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now
+  )
 end
