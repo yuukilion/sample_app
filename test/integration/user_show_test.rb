2032d099fe3220ff6a7b1e91b63525ad7813ba69
yuukilion <hy130310@gmail.com>
2023-09-04 08:46:29 +0000

    [add]テストを追加

--- test/integration/user_show_test.rb
+++ test/integration/user_show_test.rb
@@ -3,12 +3,18 @@ require "test_helper"
 class UserShowTest < ActionDispatch::IntegrationTest
   def setup
     @inactive_user = users(:inactive)
-    @active_user = users(:archer)
+    @activated_user = users(:archer)
   end
 
   test "should redirect when user not activated" do
     get user_path(@inactive_user)
-    assert_response 
-    assert_redirected_to
+    assert_response :redirect
+    assert_redirected_to root_url
+  end
+
+  test "should display user when activated" do
+    get user_path(@activated_user)
+    assert_response :success
+    assert_template 'users/show'
   end
 end
