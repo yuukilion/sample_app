58802dce98bc84437b0ee3fdedf9fa53b1cbe03c
yuukilion <hy130310@gmail.com>
2023-09-05 08:31:17 +0000

    [add]パスワード再設定用メール設定のテストを追加

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -12,4 +12,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.activation_token, mail.body.encoded
     assert_match CGI.escape(user.email), mail.body.encoded
   end
+
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "【SampleApp】パスワード再設定", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["sampleTarou123456@gmail.com"],mail.from
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
+  end
 end
