4324ba1fd6d53f93fa095fde031bffac00f3ed25
yuukilion <hy130310@gmail.com>
2023-09-06 05:01:20 +0000

    [add]統合テストを追加

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -27,8 +27,10 @@ class PasswordResetsController < ApplicationController
       @user.errors.add(:password, "Can`t be empty")
       render 'edit', status: :unprocessable_entity
     elsif @user.update(user_params)
+      @user.forget
       reset_session
       log_in @user
+      @user.update_attribute(:reset_digest,nil)
       flash[:success] = "Password has been reset"
       redirect_to @user
     else
