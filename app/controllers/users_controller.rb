f683269373f5f071ae3e353ee7dce5231d7419d2
yuukilion <hy130310@gmail.com>
2023-09-07 12:30:36 +0000

    [update]マイクロポストのアクセス制御を実施

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,14 +56,6 @@ class UsersController < ApplicationController
 
     #beforeフィルター
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in"
-        redirect_to login_url, status: :see_other
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url, status: :see_other) unless current_user?(@user)
